3b9f8a32c4b2fc850983cd05f651098255e374a8
ribkacta01 <ribkacta29@gmail.com>
2023-02-21 22:06:15 +0700

    tulisan header aja

--- lib/app/modules/keranjang/views/keranjang_view.dart
+++ lib/app/modules/keranjang/views/keranjang_view.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
 
 import 'package:get/get.dart';
+import 'package:hexcolor/hexcolor.dart';
+import 'package:phosphor_flutter/phosphor_flutter.dart';
+import 'package:sizer/sizer.dart';
 
 import '../controllers/keranjang_controller.dart';
 
@@ -9,16 +12,55 @@ class KeranjangView extends GetView<KeranjangController> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('KeranjangView'),
-        centerTitle: true,
-      ),
-      body: Center(
-        child: Text(
-          'KeranjangView is working',
-          style: TextStyle(fontSize: 20),
-        ),
-      ),
-    );
+        body: SingleChildScrollView(
+            padding: EdgeInsets.only(
+              left: 20,
+              right: 20,
+              top: 6,
+            ),
+            child: Column(
+                mainAxisAlignment: MainAxisAlignment.start,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                textDirection: TextDirection.ltr,
+                children: [
+                  SizedBox(height: 8.h),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Column(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(
+                            "Halo Ribka",
+                            style: TextStyle(
+                              fontSize: 19,
+                              color: HexColor("#0B0C2B"),
+                              fontWeight: FontWeight.w500,
+                            ),
+                          ),
+                          SizedBox(height: 1.h),
+                          Text(
+                            "Mulai Perencanaan Bahan Baku ",
+                            style: TextStyle(
+                              fontSize: 19,
+                              color: HexColor("#0B0C2B"),
+                              fontWeight: FontWeight.w500,
+                            ),
+                          ),
+                        ],
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.only(right: 5),
+                        child: Icon(
+                          PhosphorIcons.userCircle,
+                          size: 45,
+                          color: HexColor("#0B0C2B"),
+                        ),
+                      ),
+                    ],
+                  ),
+                ])));
   }
 }
--- lib/app/modules/profil/views/profil_view.dart
+++ lib/app/modules/profil/views/profil_view.dart
@@ -15,56 +15,59 @@ class ProfilView extends GetView<ProfilController> {
     final authC = Get.put(AuthController());
     return Scaffold(
         body: SingleChildScrollView(
-            padding: EdgeInsets.only(
-              left: 20,
-              right: 20,
-              top: 6,
-            ),
-            child: Column(
-                mainAxisAlignment: MainAxisAlignment.start,
+      padding: EdgeInsets.only(
+        left: 20,
+        right: 20,
+        top: 6,
+      ),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.start,
+        crossAxisAlignment: CrossAxisAlignment.start,
+        textDirection: TextDirection.ltr,
+        children: [
+          SizedBox(height: 8.h),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Column(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
                 crossAxisAlignment: CrossAxisAlignment.start,
-                textDirection: TextDirection.ltr,
                 children: [
-                  SizedBox(height: 8.h),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Column(
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          Text(
-                            "Halo Ribka",
-                            style: TextStyle(
-                              fontSize: 19,
-                              color: HexColor("#0B0C2B"),
-                              fontWeight: FontWeight.w500,
-                            ),
-                          ),
-                          SizedBox(height: 1.h),
-                          Text(
-                            "Halaman Profil Anda",
-                            style: TextStyle(
-                              fontSize: 19,
-                              color: HexColor("#0B0C2B"),
-                              fontWeight: FontWeight.w500,
-                            ),
-                          ),
-                        ],
-                      ),
-                      Padding(
-                          padding: const EdgeInsets.only(right: 0),
-                          child: IconButton(
-                              onPressed: () {
-                                authC.logout();
-                              },
-                              icon: Icon(
-                                PhosphorIcons.signOut,
-                                color: HexColor("#D90000"),
-                              ))),
-                    ],
+                  Text(
+                    "Halo Ribka",
+                    style: TextStyle(
+                      fontSize: 19,
+                      color: HexColor("#0B0C2B"),
+                      fontWeight: FontWeight.w500,
+                    ),
                   ),
-                ])));
+                  SizedBox(height: 1.h),
+                  Text(
+                    "Halaman Profil Anda",
+                    style: TextStyle(
+                      fontSize: 19,
+                      color: HexColor("#0B0C2B"),
+                      fontWeight: FontWeight.w500,
+                    ),
+                  ),
+                ],
+              ),
+              Padding(
+                  padding: const EdgeInsets.only(right: 0),
+                  child: IconButton(
+                      onPressed: () {
+                        authC.logout();
+                      },
+                      icon: Icon(
+                        PhosphorIcons.signOut,
+                        color: HexColor("#D90000"),
+                      ))),
+            ],
+          ),
+          Container()
+        ],
+      ),
+    ));
   }
 }
